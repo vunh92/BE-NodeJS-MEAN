50064cc772fca1450a732657b2e6eb875370cd7a
Nguyen Huu Vu <nguyen.huu.vu@logigear.tokyo>
2022-02-28 12:40:48 +0700

    fix public

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 // cho phép domain sử dụng api
 app.use((req, res, next)=>{
